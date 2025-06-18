35334e37b9277a5e765e5da3910871d9ff2c4a4a
PhonSobon <phon.sobon02@gmail.com>
2025-06-18 15:31:22 +0700

    fix buge metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,36 +19,31 @@ const kantumruyPro = Kantumruy_Pro({
 });
 
 export const metadata = {
-  metadataBase: new URL(
-    process.env.NEXT_PUBLIC_APP_URL || "https://kha-com.vercel.app"
-  ),
-  manifest:
-    (process.env.NEXT_PUBLIC_APP_URL || "https://kha-com.vercel.app") +
-    "/images/Logo/logo.jpg",
-  title: {
-    template: "%s - KHMER HEIRS ASSOCIATION",
-  },
+  title: "KHMER HEIRS ASSOCIATION",
+  description:
+    "The Khmer Heirs Association is a non-governmental organization that is independent, sovereign, apolitical, non-partisan, and not for private profit.",
   openGraph: {
-    title: "Home - KHMER HEIRS ASSOCIATION",
-    description: "Collect better data and make better decisions.",
-    url: "/",
+    title: "KHMER HEIRS ASSOCIATION",
+    description:
+      "The Khmer Heirs Association is a non-governmental organization that is independent, sovereign, apolitical, non-partisan, and not for private profit.",
+    url: "https://kha-com.vercel.app/",
     siteName: "KHMER HEIRS ASSOCIATION",
     images: [
       {
-        url: "/og-image.jpg",
-        width: 800,
-        height: 600,
+        url: "https://kha-com.vercel.app/og-image.jpg", // Absolute URL required!
+        width: 1200,
+        height: 630,
+        alt: "KHMER HEIRS ASSOCIATION",
       },
     ],
+    type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    title: {
-      template: "%s - KHMER HEIRS ASSOCIATION",
-    },
-    description: "Collect better data and make better decisions.",
-    creator: "KHMER HEIRS ASSOCIATION",
-    images: ["/og-image.jpg"],
+    title: "KHMER HEIRS ASSOCIATION",
+    description:
+      "The Khmer Heirs Association is a non-governmental organization that is independent, sovereign, apolitical, non-partisan, and not for private profit.",
+    images: ["https://kha-com.vercel.app/og-image.jpg"],
   },
 };
 
